f197f2cdc49136624a371fefdd7641dcc75537b0
Brian Butterly <butterlybrian@gmail.com>
2020-07-01 22:45:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,12 +11,14 @@ def prompt_user
 end
 
 def selection(num)
+  num = 2
   if num = 1
     binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
     binding.pry
      "HAM HAM HAM IN MY TUMMY"
+     
   end
 end
 
